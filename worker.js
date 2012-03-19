2710009dd20e207e59808e29e4458822fee35824
benmmurphy <benmmurphy@gmail.com>
2012-03-19 15:34:52 +0000

    randomize time

--- worker.js
+++ worker.js
@@ -1,8 +1,9 @@
 process.on("message", function(msg) {
   console.log("worker received msg: ", msg);
+  var timeout = Math.random() * 1000;
   setTimeout(function() {
     var result = {id: msg.id, response: "ECHO: " + msg.query.echo};
     console.log("sending result: ", result);
     process.send(result);
-  }, 500);
+  }, timeout);
 });
